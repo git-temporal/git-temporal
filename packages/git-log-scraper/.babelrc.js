db21348b52ef09798a67decc87b0c8ea86fd903c
Bee <beewilkerson@invisionapp.com>
2018-09-23 13:54:19 -0700

    git-log-scraper: use babel to compile ts

--- packages/git-log-scraper/.babelrc.js
+++ packages/git-log-scraper/.babelrc.js
@@ -8,15 +8,15 @@ module.exports = {
         },
       },
     ],
-    [
-      '@babel/stage-1',
-      {
-        decoratorsLegacy: true,
-      },
-    ],
     '@babel/typescript',
   ],
   plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-logical-assignment-operators',
+    ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+    '@babel/plugin-proposal-do-expressions',
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
     [
--- scripts/addDevDependencies.sh
+++ scripts/addDevDependencies.sh
@@ -2,37 +2,42 @@
 
 npm install
 
-npm install --save-dev @babel/cli@7.1.0 \
-@babel/core@7.0.0-beta.51 \
-@babel/plugin-proposal-class-properties@7.0.0-beta.51 \
-@babel/plugin-proposal-object-rest-spread@7.0.0-beta.51 \
-@babel/plugin-transform-modules-commonjs@7.0.0-rc.3 \
-@babel/preset-env@7.0.0-beta.51 \
-@babel/preset-stage-1@7.0.0-beta.51 \
-@babel/preset-typescript@7.0.0-beta.51 \
-@babel/register@7.0.0-rc.1 \
-@types/node@10.3.3 \
-babel-eslint@8.2.6 \
-babel-jest@23.4.2 \
-babel-loader@8.0.0-beta.2 \
-babel-plugin-module-resolver@3.1.1 \
-bumble-util@0.4.1 \
-chai@4.1.2 \
-cross-env@5.2.0 \
-doctoc@1.3.1 \
-eslint@4.19.1 \
-eslint-config-airbnb@17.0.0 \
-eslint-config-prettier@2.9.0 \
-eslint-plugin-import@2.13.0 \
-husky@0.14.3 \
-mocha@5.2.0 \
-prettier@1.13.5 \
-regenerator-runtime@0.12.1 \
-ts-node@6.1.1 \
-tsconfig-paths@3.5.0 \
-tslint@5.11.0 \
-tslint-config-airbnb@5.9.2 \
-tslint-config-prettier@1.13.0 \
-typescript@3.0.3 \
-underscore@1.9.1 
+npm install --save-dev @babel/cli \
+@babel/core \
+@babel/plugin-proposal-class-properties \
+@babel/plugin-proposal-object-rest-spread \
+@babel/plugin-proposal-export-default-from \
+@babel/plugin-proposal-logical-assignment-operators \
+@babel/plugin-proposal-optional-chaining \
+@babel/plugin-proposal-pipeline-operator \
+@babel/plugin-proposal-nullish-coalescing-operator \
+@babel/plugin-proposal-do-expressions \
+@babel/plugin-transform-modules-commonjs \
+@babel/preset-env \
+@babel/preset-typescript \
+@babel/register \
+@types/node \
+babel-eslint \
+babel-jest \
+babel-loader \
+babel-plugin-module-resolver \
+bumble-util \
+chai \
+cross-env \
+doctoc \
+eslint \
+eslint-config-airbnb \
+eslint-config-prettier \
+eslint-plugin-import \
+husky \
+mocha \
+prettier \
+regenerator-runtime \
+ts-node \
+tsconfig-paths \
+tslint \
+tslint-config-airbnb \
+tslint-config-prettier \
+typescript \
+underscore \
 
