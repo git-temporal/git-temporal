2c0a58f173f357be8de571211576d1efd9c01aeb
Bee <beewilkerson@invisionapp.com>
2018-10-03 14:57:33 -0700

    add test for styles

---
+++ packages/git-temporal-react/src/app/styles/index.test.ts
@@ -0,0 +1,53 @@
+import { style, replaceGlobalStyles } from 'app/styles';
+
+replaceGlobalStyles({
+  page: {
+    margin: 20,
+    backgroundColor: 'black',
+  },
+  panel: {
+    backgroundColor: 'white',
+    marginRight: 10,
+    marginBottom: 10,
+    padding: 100,
+  },
+  altPanel: {
+    _extends: 'panel',
+    backgroundColor: 'greyaf',
+  },
+  altPanelPage: {
+    _extends: ['altPanel', 'page'],
+  },
+});
+
+describe('app/styles', () => {
+  describe('when calling style()', () => {
+    test("with 'altPanel', it should merged altPanel and panel styles", () => {
+      const styleOut: any = style('altPanel');
+      expect(styleOut.backgroundColor).toBe('greyaf');
+      expect(styleOut.padding).toBe(100);
+    });
+    describe("with ('altPanel', 'page') and ('altPanelPage')", () => {
+      let styleOutFromSeparate: any;
+      let styleOutFromSingle: any;
+      beforeAll(() => {
+        styleOutFromSeparate = style('altPanel', 'page');
+        debugger;
+        styleOutFromSingle = style('altPanelPage');
+      });
+      test('the two outputs should equal each other', () => {
+        expect(styleOutFromSeparate).toEqual(styleOutFromSingle);
+      });
+      test("it should give precedence to the last style specified ('page')", () => {
+        expect(styleOutFromSeparate.backgroundColor).toBe('black');
+        expect(styleOutFromSingle.backgroundColor).toBe('black');
+      });
+    });
+    test("with ('altPanel', 'page', {custom}), it should give precedence to last (custom)", () => {
+      const styleOut: any = style('altPanel', 'page', {
+        backgroundColor: 'chartreuse',
+      });
+      expect(styleOut.backgroundColor).toBe('chartreuse');
+    });
+  });
+});
