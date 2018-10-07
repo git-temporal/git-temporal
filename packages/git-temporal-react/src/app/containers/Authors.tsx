1dfc8dea761bf5501ec070dc7e08717b5fa3fa50
Bee <beewilkerson@invisionapp.com>
2018-10-07 09:33:40 -0700

    fix api build

--- packages/git-temporal-react/src/app/containers/Authors.tsx
+++ packages/git-temporal-react/src/app/containers/Authors.tsx
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { List } from 'react-virtualized';
 
 import { DispatchProps } from 'app/interfaces';
-import { getFilteredStats } from 'app/selectors';
+import { getAuthorsAndCommits } from 'app/selectors';
 import { style } from 'app/styles';
 import { AuthorGravatarImage } from 'app/components/AuthorGravatarImage';
 
 interface AuthorsProps {
-  minAuthorDate?: number;
-  maxAuthorDate?: number;
-  authors?: number;
-  commits?: number;
-  files?: number;
-  linesAdded?: number;
-  linesDeleted?: number;
+  authors?: object[];
 }
 
 export class Authors extends Component<AuthorsProps & DispatchProps> {
   render() {
     return (
       <div style={style('panel', 'flexRows')}>
+        <div className="list">
+          <List
+            width={235}
+            height={300}
+            rowHeight={90}
+            rowRenderer={this.renderRow}
+            rowCount={this.props.authors.length}
+          />
+        </div>
         <span>Authors render here</span>
         <AuthorGravatarImage email="bee.wilkerson@ymail.com" />
         <AuthorGravatarImage email="beewilkerson@invisionapp.com" />
       </div>
     );
   }
+  renderRow(author) {
+    return <div style={style('panel', 'flexRows')}>{author.authorName}</div>;
+  }
 }
 
 export const mapStateToProps = state => {
-  return getFilteredStats(state);
+  return { authors: getAuthorsAndCommits(state) };
 };
 
 export default connect(mapStateToProps)(Authors);
